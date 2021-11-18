658e355d3767eae608d6910b17564013d675a17b
Aryan Majeed <aryanmajid97@gmail.com>
2021-11-18 15:46:58 +0300

    build:add heroku

--- expressApp.js
+++ expressApp.js
@@ -1,23 +1,28 @@
-const express = require('express')
-const app = express()
-const auth = require('./middlewares/auth')
-
+const express = require('express');
+const app = express();
+const auth = require('./middlewares/auth');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(require("cors")());
+app.use(require('cors')());
 
-app.get("/authenticate", auth, (req, res) => {
-    res.json({ message: "verified" })
-})
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  });
+}
 
-app.use("/request", require("./routes/request"));
-app.use("/category", require("./routes/category"));
-app.use("/user", require("./routes/user"));
-app.use("/search", require("./routes/search"));
-app.use("/cart", require("./routes/cart"));
-app.use("/discount", require("./routes/discount"));
+app.get('/authenticate', auth, (req, res) => {
+  res.json({ message: 'verified' });
+});
 
+app.use('/request', require('./routes/request'));
+app.use('/category', require('./routes/category'));
+app.use('/user', require('./routes/user'));
+app.use('/search', require('./routes/search'));
+app.use('/cart', require('./routes/cart'));
+app.use('/discount', require('./routes/discount'));
 
-module.exports = app
+module.exports = app;
